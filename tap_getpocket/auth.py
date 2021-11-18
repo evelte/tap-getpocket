5214ab5d57b2445248a10ac7d0aed6df505ecaaf
elenavelte <elena@lovelystay.com>
2021-11-18 14:21:01 +0000

    update authorization header and parameters

--- tap_getpocket/auth.py
+++ tap_getpocket/auth.py
@@ -2,30 +2,35 @@
 
 
 from singer_sdk.authenticators import OAuthAuthenticator, SingletonMeta
+import logging
 
+logger = logging.getLogger(__name__)
 
 # The SingletonMeta metaclass makes your streams reuse the same authenticator instance.
 # If this behaviour interferes with your use-case, you can remove the metaclass.
 class GetPocketAuthenticator(OAuthAuthenticator, metaclass=SingletonMeta):
-    """Authenticator class for GetPocket."""
+    """Authenticator class for GetPocket.."""
+
+    # meltano config tap-getpocket list
 
     @property
-    def oauth_request_body(self) -> dict:
-        """Define the OAuth request body for the GetPocket API."""
-        # TODO: Define the request body needed for the API.
-        return {
-            'resource': 'https://analysis.windows.net/powerbi/api',
-            'scope': self.oauth_scopes,
-            'client_id': self.config["client_id"],
-            'username': self.config["username"],
-            'password': self.config["password"],
-            'grant_type': 'password',
-        }
+    def auth_params(self) -> dict:
+        # access token in config.json is only found if explicitly invoked
+        # otherwise this is always reading from meltano.yml
+        if not self.config.get('access_token'):
+            logger.critical('No access token available. Run authentication flow according to README')
+        return {"consumer_key": self.config.get('consumer_key'),
+                "access_token": self.config.get('access_token')
+                }
+
+    @property
+    def auth_headers(self) -> dict:
+        return {'Content-Type': 'application/json',
+                "X-Accept": "application/json",
+                "charset": 'utf-8'}
 
     @classmethod
     def create_for_stream(cls, stream) -> "GetPocketAuthenticator":
         return cls(
-            stream=stream,
-            auth_endpoint="TODO: OAuth Endpoint URL",
-            oauth_scopes="TODO: OAuth Scopes",
+            stream=stream
         )
