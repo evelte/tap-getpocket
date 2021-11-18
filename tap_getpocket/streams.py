602987e555851de85d8efb0779a67b3a646b856b
elenavelte <elena@lovelystay.com>
2021-11-18 14:24:45 +0000

    add content stream

--- tap_getpocket/streams.py
+++ tap_getpocket/streams.py
@@ -12,51 +12,43 @@ SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 # TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
 #       - Copy-paste as many times as needed to create multiple stream types.
 
-
-class UsersStream(GetPocketStream):
+class ContentStream(GetPocketStream):
     """Define custom stream."""
-    name = "users"
-    path = "/users"
-    primary_keys = ["id"]
-    replication_key = None
-    # Optionally, you may also use `schema_filepath` in place of `schema`:
-    # schema_filepath = SCHEMAS_DIR / "users.json"
+    name = "list"
+    path = "/./"
+    primary_keys = ["item_id"]
+    replication_key = "time_updated"
+    # Schemas solve the limited data types problem by providing more information about how to interpret JSON's basic types
     schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property(
-            "id",
-            th.StringType,
-            description="The user's system ID"
-        ),
-        th.Property(
-            "age",
-            th.IntegerType,
-            description="The user's age in years"
-        ),
-        th.Property(
-            "email",
-            th.StringType,
-            description="The user's email address"
-        ),
-        th.Property("street", th.StringType),
-        th.Property("city", th.StringType),
-        th.Property(
-            "state",
-            th.StringType,
-            description="State name in ISO 3166-2 format"
-        ),
-        th.Property("zip", th.StringType),
-    ).to_dict()
-
-
-class GroupsStream(GetPocketStream):
-    """Define custom stream."""
-    name = "groups"
-    path = "/groups"
-    primary_keys = ["id"]
-    replication_key = "modified"
-    schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property("id", th.StringType),
-        th.Property("modified", th.DateTimeType),
+        th.Property("item_id", th.StringType, required=True),
+        th.Property("resolved_id", th.StringType),
+        th.Property("given_url", th.StringType),
+        th.Property("resolved_url", th.StringType),
+        th.Property("given_title", th.StringType),
+        th.Property("resolved_title", th.StringType),
+        th.Property("favorite", th.StringType),
+        th.Property("status", th.StringType),
+        th.Property("excerpt", th.StringType),
+        th.Property("is_article", th.BooleanType),
+        th.Property("has_image", th.BooleanType),
+        th.Property("has_video", th.BooleanType),
+        th.Property("word_count", th.StringType),
+        th.Property("time_updated", th.DateTimeType, default='0'),
+        th.Property("time_added", th.DateTimeType),
+        th.Property("time_read", th.DateTimeType),
+        th.Property("time_favorited", th.DateTimeType),
+        th.Property("sort_id", th.IntegerType),
+        th.Property("is_index", th.BooleanType),
+        th.Property("lang", th.StringType),
+        th.Property("listen_duration_estimate", th.IntegerType),
+        th.Property("time_to_read", th.IntegerType),
+        th.Property("amp_url", th.StringType),
+        th.Property("top_image_url", th.StringType),
+        th.Property("domain_metadata",
+                    th.ObjectType(
+                        th.Property("greyscale_logo", th.StringType),
+                        th.Property("logo", th.StringType),
+                        th.Property("name", th.StringType),
+                                )
+                    ),
     ).to_dict()
